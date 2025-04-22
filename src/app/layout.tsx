fdefe85b164904df6ffecb31cb6bed36a4ad2e3a
Business Konto <Businesskonto@Mac.fritz.box>
2025-04-22 20:00:18 +0200

    Google Analytics ID aktualisiert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <head>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-YSHFRNTY56`}
         />
         <Script
           id="gtag-init"
@@ -37,7 +37,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              gtag('config', 'G-YSHFRNTY56', {
                 page_path: window.location.pathname,
               });
             `,
